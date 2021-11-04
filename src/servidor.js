0ae8faef8113336ab626b5f566909efdd81071e3
geanroberto <gean_roberto@hotmail.com>
2021-11-04 09:03:21 -0300

    Adicionando porta para acessar pelo Postman

--- src/servidor.js
+++ src/servidor.js
@@ -1,6 +1,12 @@
+var porta = 3003;
+
 var express = require('express');
 var app = express();
 
 app.get('/produtos', function (req, res) {
   res.send({ nome: 'Escova de dente', preco: 5.99 })
+})
+
+app.listen(porta, function () {
+  console.log(`Servidor rodando na porta ${porta}.`);
 })
