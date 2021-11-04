c5b094b11d50a1c4bc418e222158b2da735d7bba
geanroberto <gean_roberto@hotmail.com>
2021-11-04 10:06:20 -0300

    Integração das funções middlewares

--- src/servidor.js
+++ src/servidor.js
@@ -2,9 +2,22 @@ var porta = 3003;
 
 var express = require('express');
 var app = express();
+var bancoDeDados = require('./bancoDeDados');
 
 app.get('/produtos', function (req, res) {
-  res.send({ nome: 'Escova de dente', preco: 5.99 })
+  res.send(bancoDeDados.getProdutos());
+})
+
+app.get('/produtos/:id', function (req, res) {
+  res.send(bancoDeDados.getProduto(req.params.id));
+})
+
+app.post('/produtos', function (req, res) {
+  const produto = bancoDeDados.salvarProduto({
+    nome: req.body.nome,
+    preco: req.body.preco
+  })
+  res.send(produto);
 })
 
 app.listen(porta, function () {
